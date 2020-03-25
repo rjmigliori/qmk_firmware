556b29a33b64a500b2b1f0cea061d3474f4dfa1b
Purdea Andrei <andrei@purdea.ro>
2020-03-25 08:04:06 +0200

    modelfkeyboards.com: setting dummy col pins which exist on atmega32u2 to allow compilation

--- keyboards/xwhatsit/modelfkeyboards.com/config.h
+++ keyboards/xwhatsit/modelfkeyboards.com/config.h
@@ -42,7 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_COL_PINS { B2, B1, B0 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
