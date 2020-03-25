332fa7c4f8102ebcdf17c24507edf4b486bc70e3
Purdea Andrei <andrei@purdea.ro>
2020-03-25 10:32:26 +0200

    xwhatsit/modelfkeyboards_com: enable custom matrix

--- keyboards/xwhatsit/modelfkeyboards_com/config.h
+++ keyboards/xwhatsit/modelfkeyboards_com/config.h
@@ -41,12 +41,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { B2, B1, B0 }
-#define UNUSED_PINS
+//#define MATRIX_ROW_PINS { D0, D5 }
+//#define MATRIX_COL_PINS { B2, B1, B0 }
+//#define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
-#define DIODE_DIRECTION COL2ROW
+//#define DIODE_DIRECTION COL2ROW
 
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
