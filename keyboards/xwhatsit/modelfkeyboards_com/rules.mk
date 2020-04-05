695a637747f00c9bfc6725da965229e707c44ebb
Purdea Andrei <andrei@purdea.ro>
2020-04-05 17:21:03 +0300

    xwhatsit/modelfkeyboards_com: moved matrix.c into common xwhatsit folder

--- keyboards/xwhatsit/modelfkeyboards_com/rules.mk
+++ keyboards/xwhatsit/modelfkeyboards_com/rules.mk
@@ -32,4 +32,4 @@ FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 
 CUSTOM_MATRIX=lite
-SRC += matrix.c
+SRC += keyboards/xwhatsit/matrix.c
