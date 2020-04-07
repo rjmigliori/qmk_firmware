61ef949a3490252ba4ca59bcc533aaac44ac7c5c
Purdea Andrei <andrei@purdea.ro>
2020-04-07 13:22:16 +0300

    xwhatsit/brand_new_model_f_keyboards/*: LAYOUT_all: also expose hidden key under space.

--- keyboards/xwhatsit/brand_new_model_f_keyboards/f77/f77.h
+++ keyboards/xwhatsit/brand_new_model_f_keyboards/f77/f77.h
@@ -37,13 +37,13 @@
     k_tab,   k_q, k_w, k_e, k_r, k_t, k_y, k_u, k_i, k_o, k_p, k_squarebrop, k_squarebrcl,k_backspace,       k_kp4, k_kp5, k_kp6,\
     k_lctrl,   k_a, k_s, k_d, k_f, k_g, k_h, k_j, k_k, k_l, k_semicolon, k_singlequote, k_hashtag, k_return, k_kp1, k_kp2, k_kp3,\
     k_lshift, k_nonusbs, k_z, k_x, k_c, k_v, k_b, k_n, k_m, k_cm,k_period, k_forwardslash, k_rshift, k_fn,   k_kp0, k_up,  k_kpperdel,\
-    k_caps, k_lgui, k_lalt,         k_space,                 k_ralt                , k_numlock, k_rctrl,     k_left,k_down,k_right\
+    k_caps, k_lgui, k_lalt,         k_space,                     k_extrakey, k_ralt, k_numlock, k_rctrl,     k_left,k_down,k_right\
 ) \
 { \
     { k_nonusbs,k_x,    k_d, k_g,     k_k, k_l,         k_forwardslash, k_rshift,     k_kp0,   k_up,       KC_NO},\
     { k_lgui,   k_z,    k_c, k_b,     k_n, k_period,    k_ralt,         k_fn,         k_left,  k_kpperdel, KC_NO},\
     { k_lshift, k_s,    k_f, k_h,     k_j, k_semicolon, k_singlequote,  k_return,     k_kp1,   k_kp3,      KC_NO},\
-    { k_caps,   k_lalt, k_v, k_space, k_m, k_cm,        KC_NO,          k_numlock,    k_rctrl, k_down,     k_right},\
+    { k_caps,   k_lalt, k_v, k_space, k_m, k_cm,        k_extrakey,     k_numlock,    k_rctrl, k_down,     k_right},\
     { k_esc,    k_q,    k_3, k_5,     k_7, k_9,         k_minus,        k_backslash,  KC_NO,   k_kp8,      KC_NO},\
     { k_tab,    k_w,    k_r, k_y,     k_i, k_p,         k_squarebrop,   k_squarebrcl, k_kp4,   k_kp6,      KC_NO},\
     { k_1,      k_2,    k_4, k_6,     k_8, k_0,         k_equals,       k_backtick,   k_kp7,   k_kp9,      KC_NO},\
