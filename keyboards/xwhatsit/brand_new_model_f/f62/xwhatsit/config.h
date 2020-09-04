0f6295d3b4a0474f4d001046ff88003e9c559b03
Purdea Andrei <andrei@purdea.ro>
2020-09-04 06:11:07 +0300

    brand_new_model_f: fixing typo in product descriptor string

--- keyboards/xwhatsit/brand_new_model_f/f62/xwhatsit/config.h
+++ keyboards/xwhatsit/brand_new_model_f/f62/xwhatsit/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID 0x0002
 #define DEVICE_VER 0x0001
 #define MANUFACTURER Tom Wong-Cornall/Ellipse/Purdea Andrei
-#define PRODUCT modelfkeyboards.com Brand New F62/F77 Model F Keybard by Model F Labs
+#define PRODUCT modelfkeyboards.com Brand New F62/F77 Model F Keyboard by Model F Labs
 #define DESCRIPTION QMK firmware for the modelfkeyboards.com reproduction of the IBM Model F keyboards using the original Xwhatsit model F controller PCB
 
 /* key matrix size */
