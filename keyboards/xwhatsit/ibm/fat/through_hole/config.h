e5f57ee710842895b4e32ee4dc21d0f3e726dba1
Purdea Andrei <andrei@purdea.ro>
2020-10-19 05:03:37 +0300

    All model Fs: double the threshold offset used.

--- keyboards/xwhatsit/ibm/fat/through_hole/config.h
+++ keyboards/xwhatsit/ibm/fat/through_hole/config.h
@@ -268,7 +268,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define CAPSENSE_CAL_INIT_REPS 16
 #define CAPSENSE_CAL_EACHKEY_REPS 16
 #define CAPSENSE_CAL_BINS 5
-#define CAPSENSE_CAL_THRESHOLD_OFFSET 48
+#define CAPSENSE_CAL_THRESHOLD_OFFSET 96
 
 #if !CAPSENSE_CAL_ENABLED
 #define CAPSENSE_HARDCODED_THRESHOLD 142
